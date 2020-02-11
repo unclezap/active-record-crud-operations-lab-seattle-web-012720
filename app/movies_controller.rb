7043073884768eb2f6211bd48a38c98e4f71b9e2
unclezap <zeb.blackwell@gmail.com>
2020-02-10 16:25:50 -0800

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -4,15 +4,18 @@
 # def make_a_new_movie_instance    # def make_a_new_movie_instance
 #   movie = __                     #   movie = Movie.new
 # end                              # end
-
+require 'pry'
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = Movie.new
+  # binding.pry
   movie.title = "This is a title."
-  __
+  # binding.pry
+  movie.save
 end
 
 def can_be_created_with_a_hash_of_attributes
   # Initialize movie and then and save it
+  
   attributes = {
       title: "The Sting",
       release_date: 1973,
@@ -20,33 +23,41 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
-  
+  # binding.pry
   Movie.create do |m|
-    __
+    # binding.pry
+    m.title = args[:title]
+    m.release_date = args[:release_date]
+    # binding.pry
   end
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  Movie.all[0]
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.all[-1]
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.all.length
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  # binding.pry
+  Movie.all.find {|m| m[:id] == 1}
+  # binding.pry
+  # first.title = first[0][:title]
+  # first
+  # binding.pry
 end
 
 def can_find_by_multiple_attributes
@@ -54,28 +65,34 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  Movie.all.find {|m| m[:title] == "Title" && m[:release_date] == 2000 && m[:director] == "Me"}
 end
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  __
+  list = Movie.all.select {|m| m[:release_date] > 2002}
+  list.sort_by {|m| -m[:release_date]}
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
+  # binding.pry
   Movie.create(title: "Awesome Flick")
-  __
-  __
-  __
+  list = Movie.find {|m| m[:title] == "Awesome Flick"}
+  list.title = "Even Awesomer Flick"
+  list.save
+  # new_movie.save
+  # new_movie
 end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+  list = Movie.find {|m| m[:title] == "Wat?"}
+  list.title = "Wat, huh?"
+  list.save
+  
 end
 
 def can_update_multiple_items_at_once
@@ -83,18 +100,26 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  __
+  # binding.pry
+  Movie.all.each do |m|
+    m[:title] = "A Movie"
+    m.save
+  end
+
 end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
-  __
+  Movie.all.find do |m|
+    m[:title] = "That One Where the Guy Kicks Another Guy Once"
+    m.delete
+  end
+  
 end
 
 def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  __
+  Movie.all.map {|m| m.delete}
 end
